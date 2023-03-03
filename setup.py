50cbc805af0a8390e4ab2d8480bdda6b5b320770
lenniDoesMath <121552735+lenniDoesMath@users.noreply.github.com>
2023-03-03 22:18:44 +0100

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+  
+setup(
+    name='planarDiagram',
+    version='0.1',
+    description='A implementation for knot theory, planar diagrams, and reidemeister moves',
+    author='lenniDoesMath',
+    author_email='lenniDoesMath@gmail.com',
+    packages=['planarDiagram'],
+    install_requires=[
+    ],
+)
